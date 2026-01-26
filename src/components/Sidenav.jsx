f2fff61f2ca4db8a57f83382568f17ae31baf11c
JestinGigi <jestingigi@gmail.com>
2026-01-26 21:18:30 +0530

    feat: Added tooltips on all the icons.

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { AiOutlineHome, AiOutlineMenu, AiOutlineProject, AiOutlineMail} from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineMenu, AiOutlineProject, AiOutlineMail } from 'react-icons/ai';
 import { BsPerson } from 'react-icons/bs';
 import { GrProjects } from 'react-icons/gr';
+import { Tooltip } from "react-tooltip";
 
 const Sidenav = () => {
     const [nav, setNav] = useState(false);
@@ -33,33 +34,38 @@ const Sidenav = () => {
                     <a onClick={handleNav} href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineMail size={20} />
                         <span className="pl-4">Contact</span>
-                    </a>    
+                    </a>
                 </div>
             ) :
                 (
                     ''
                 )
-            } 
+            }
 
             <div className="md:block hidden fixed top-[25%] left-0 z-10">
                 <div className='flex flex-col'>
                     <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-4 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                    <AiOutlineHome size={20} />
+                        <AiOutlineHome data-tooltip-id="home-tooltip" className="cursor-pointer" size={20} />
+                        <Tooltip id="home-tooltip" place="right" content="Home" />
                     </a>
                     <a href="#Work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-4 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                    <GrProjects size={20} />
+                        <GrProjects data-tooltip-id="work-tooltip" className="cursor-pointer" size={20} />
+                        <Tooltip id="work-tooltip" place="right" content="Work" />
                     </a>
                     <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-4 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                    <AiOutlineProject size={20} />
+                        <AiOutlineProject data-tooltip-id="projects-tooltip" className="cursor-pointer" size={20} />
+                        <Tooltip id="projects-tooltip" place="right" content="Projects" />
                     </a>
                     <a href="#resume" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-4 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                    <BsPerson size={20} />
+                        <BsPerson data-tooltip-id="resume-tooltip" className="cursor-pointer" size={20} />
+                        <Tooltip id="resume-tooltip" place="right" content="Resume" />
                     </a>
                     <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-4 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                    <AiOutlineMail size={20} />
+                        <AiOutlineMail data-tooltip-id="contact-tooltip" className="cursor-pointer" size={20} />
+                        <Tooltip id="contact-tooltip" place="right" content="Contact" />
                     </a>
                 </div>
-            </div>      
+            </div>
         </div>
     );
 };
